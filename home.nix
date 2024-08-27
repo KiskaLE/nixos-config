b4808cffb510ce359bc8197b9badc23a21b7727e
KiskaLE <vojtakylar@seznam.cz>
2024-08-27 18:26:08 +0200

    update

--- home.nix
+++ home.nix
@@ -57,6 +57,7 @@
     enable = true;
     settings = {
       general = {
+        disable_loading_bar = true;
         grace = 300;
         hide_cursor = true;
       };
@@ -69,6 +70,20 @@
         vibrancy = 0.1696;
         vibrancy_darkness = 0.0;
       };
+      input-field =
+          {
+          size = "200, 50";
+          position = "0, -80";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = false;
+          font_color = "rgb(CFE6F4)";
+          inner_color = "rgb(657DC2)";
+          outer_color = "rgb(0D0E15)";
+          outline_thickness = 5;
+          placeholder_text = "Password...";
+          shadow_passes = 2;
+        };
     };
   };
 
