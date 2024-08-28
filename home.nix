43ed06f9eabfda782210f48441cd4655bbbbd590
KiskaLE <vojtakylar@seznam.cz>
2024-08-28 11:27:57 +0200

    hypr style

--- home.nix
+++ home.nix
@@ -254,6 +254,35 @@
         ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       ];
     };
+    extraConfig = 
+      ''
+        animations {
+            enabled = yes
+            bezier = wind, 0.05, 0.9, 0.1, 1
+            bezier = winIn, 0.1, 1, 0.1, 1
+            bezier = winOut, 0.3, -0.3, 0, 1
+            bezier = liner, 1, 1, 1, 1
+            animation = windows, 1, 6, wind, slide
+            animation = windowsIn, 1, 6, winIn, slide
+            animation = windowsOut, 1, 5, winOut, slide
+            animation = windowsMove, 1, 5, wind, slide
+            animation = border, 1, 1, liner
+            animation = fade, 1, 10, default
+            animation = workspaces, 1, 5, wind
+          }
+
+        decoration {
+            rounding = 5
+            blur {
+                enabled = true
+                size = 5
+                passes = 3
+                new_optimizations = on
+                ignore_opacity = off
+            }
+          }
+
+      '';
   };
 
   # Let Home Manager install and manage itself.
