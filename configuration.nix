03d384550d9a712b0b5703572498a83fe1d1c016
KiskaLE <vojtakylar@seznam.cz>
2024-09-04 19:48:49 +0200

    flatpak

--- configuration.nix
+++ configuration.nix
@@ -140,6 +140,17 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enable flatpak
+  services.flatpak.enable = true;
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
